f2b31937c29bec8143e487dbd9786765055e8e78
CAD97 <cad97@cad97.com>
2017-06-08 00:28:24 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,73 @@
+# Single
+
+This crate exposes a `Single` trait for extracting the element from a 
+single-element iterator (or `panic!`ing if that precondition is false).
+
+## License
+
+You may use this crate under the MIT license or the Apache License (Version 2)
+at your discretion. This crate is dual-licensed for compatibility with rust itself.
+
+## Trait single::Single
+
+```rust
+pub trait Single {
+    type Item;
+    fn single(self) -> Self::Item;
+    fn single_or(self, default: Self::Item) -> Self::Item;
+    fn single_or_else<F>(self, default_fn: F) -> Self::Item
+        where F: FnOnce() -> Self::Item;
+}
+```
+
+### Associated Types
+
+<dl>
+  <dt><code>type Item</code>
+  <dd><p>The item type of the wrapped iterator.
+</dl>
+
+### Required Methods
+
+<dl>
+  <dt><code>fn single(self) -> Self::Item</code>
+  <dd>
+    <p>Get the single element from a single-element iterator.
+    <p>Note that many iterators return references to the elements, so this method will as well if the backing iterator does.
+    <h4>Panics
+    <p>Panics if the iterator is empty or has more than one element.
+    <h4>Examples
+    <p>An emtpy iterator panics:
+    <pre>iter::empty().single();</pre>
+    <p>An iterator with multiple elements panics:
+    <pre>iter::repeat(0).single();</pre>
+    <p>An iterator with a single element returns that element:
+    <pre>assert_eq!(iter::once(0).single(), 0);</pre>
+  <dt><code>fn single_or(self, default: Self::Item) -> Self::Item</code>
+    <p>Get the single element from an iterator or a default fallback value.
+    <p>Note that many iterators return references to the elements, so this method will as well if the backing iterator does.
+    <h4>Examples
+    <p>An empty iterator will return the fallback:
+    <pre>assert_eq!(iter::empty().single_or(5), 5)</pre>
+    <p>An iterator with multiple elements will return the fallback:
+    <pre>assert_eq!(iter::repeat(0).single_or(5), 5)</pre>
+    <p>An iterator with a single element returns that element:
+    <pre>assert_eq!(iter::once(0).single_or(5), 0)</pre>
+  <dt><code>fn single_or_else<F>(self, default_fn: F) -> Self::Item where F: FnOnce() -> Self::Item</code>
+    <p>Get the single element from an iterator or from a default provider.
+    <p>Note that many iterators return references to the elements, so this method will as well if the backing iterator does.
+    <h4>Examples
+    <p>An empty iterator will return the fallback:
+    <pre>assert_eq!(iter::empty().single_or_else(|| 5), 5)</pre>
+    <p>An iterator with multiple elements will return the fallback:
+    <pre>assert_eq!(iter::repeat(0).single_or_else(|| 5), 5)</pre>
+    <p>An iterator with a single element returns that element:
+    <pre>assert_eq!(iter::once(0).single_or_else(|| 5), 0)</pre>
+</dl>
+
+### Implementors
+
+```rust
+impl<I> Single for I
+  where I: Iterator
+```
---
+++ src/lib.rs
@@ -0,0 +1,140 @@
+//! This crate exposes a `Single` trait for extracting the element from a
+//! single-element iterator (or `panic!`ing if that precondition is false).
+
+/// Trait to extract the element from a single-element iterator.
+pub trait Single {
+    /// The item type of the wrapped iterator.
+    type Item;
+
+    /// Get the single element from a single-element iterator.
+    ///
+    /// Note that many iterators return references to the elements,
+    /// so this method will as well if the backing iterator does.
+    ///
+    /// # Panics
+    ///
+    /// Panics if the iterator is empty or has more than one element.
+    ///
+    /// # Examples
+    ///
+    /// An emtpy iterator panics:
+    ///
+    /// ```should_panic(expected = "Asked empty iterator for single element")
+    /// # use single::Single;
+    /// # use std::iter;
+    /// # let _: i32 =
+    /// iter::empty().single();
+    /// ```
+    ///
+    /// An iterator with multiple elements panics:
+    ///
+    /// ```should_panic(expected = "Asked iterator with multiple elements for single element")
+    /// # use single::Single;
+    /// # use std::iter;
+    /// iter::repeat(0).single();
+    /// ```
+    ///
+    /// An iterator with a single element returns that element:
+    ///
+    /// ```
+    /// # use single::Single;
+    /// # use std::iter;
+    /// assert_eq!(iter::once(0).single(), 0);
+    /// ```
+    fn single(self) -> Self::Item;
+
+    /// Get the single element from an iterator or a default fallback value.
+    ///
+    /// Note that many iterators return references to the elements,
+    /// so this method will as well if the backing iterator does.
+    ///
+    /// # Examples
+    ///
+    /// An empty iterator will return the fallback:
+    ///
+    /// ```
+    /// # use single::Single;
+    /// # use std::iter;
+    /// assert_eq!(iter::empty().single_or(5), 5)
+    /// ```
+    ///
+    /// An iterator with multiple elements will return the fallback:
+    ///
+    /// ```
+    /// # use single::Single;
+    /// # use std::iter;
+    /// assert_eq!(iter::repeat(0).single_or(5), 5)
+    /// ```
+    ///
+    /// An iterator with a single element returns that element:
+    ///
+    /// ```
+    /// # use single::Single;
+    /// # use std::iter;
+    /// assert_eq!(iter::once(0).single_or(5), 0)
+    /// ```
+    fn single_or(self, default: Self::Item) -> Self::Item;
+
+    /// Get the single element from an iterator or from a default provider.
+    ///
+    /// Note that many iterators return references to the elements,
+    /// so this method will as well if the backing iterator does.
+    ///
+    /// # Examples
+    ///
+    /// An empty iterator will return the fallback:
+    ///
+    /// ```
+    /// # use single::Single;
+    /// # use std::iter;
+    /// assert_eq!(iter::empty().single_or_else(|| 5), 5)
+    /// ```
+    ///
+    /// An iterator with multiple elements will return the fallback:
+    ///
+    /// ```
+    /// # use single::Single;
+    /// # use std::iter;
+    /// assert_eq!(iter::repeat(0).single_or_else(|| 5), 5)
+    /// ```
+    ///
+    /// An iterator with a single element returns that element:
+    ///
+    /// ```
+    /// # use single::Single;
+    /// # use std::iter;
+    /// assert_eq!(iter::once(0).single_or_else(|| 5), 0)
+    /// ```
+    fn single_or_else<F>(self, default_fn: F) -> Self::Item
+        where F: FnOnce() -> Self::Item;
+}
+
+impl<I> Single for I where I: Iterator {
+    type Item = <Self as Iterator>::Item;
+
+    fn single(mut self) -> Self::Item {
+        match self.next() {
+            None => panic!("Asked empty iterator for single element"),
+            Some(element) => if let Some(_) = self.next() {
+                panic!("Asked iterator with multiple elements for single element")
+            } else { element }
+        }
+    }
+
+    fn single_or(self, default: Self::Item) -> Self::Item {
+        self.single_or_else(|| default)
+    }
+
+    fn single_or_else<F>(mut self, default_fn: F) -> Self::Item
+        where F: FnOnce() -> Self::Item
+    {
+        match self.next() {
+            None => default_fn(),
+            Some(element) => if let Some(_) = self.next() {
+                default_fn()
+            } else { element }
+        }
+    }
+}
+
+
